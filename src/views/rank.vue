f91a19334d0e13ab28bc1699bb031c8b1a475de6
Grace <1161937819@qq.com>
2018-07-29 09:13:52 +0800

    添加'排行'模块

--- src/views/rank.vue
+++ src/views/rank.vue
@@ -1,13 +1,34 @@
 <template>
-  <div class="hello">
-rank
+  <div class="rank">
+    <mt-cell v-for="(rank,index) in rankList" :title = "rank.rankname" :key="index" is-link>
+      <img slot="icon" :src="rank.imgurl.replace('{size}','400')" width="60" height="60">
+    </mt-cell>
   </div>
 </template>
 
 <script>
+  import rank from '../jsons/rank'
 export default {
-  name: 'rank'
+  data(){
+    return {
+      rankList: []
+    }
+  },
+  created(){
+    this.getRank()
+  },
+  methods: {
+    getRank(){
+      this.rankList = rank.rankList.list
+    }
+  }
 }
 </script>
+
+<style>
+  .rank .mint-cell-wrapper{
+    padding: 10px 5px;
+  }
+</style>
 
 
